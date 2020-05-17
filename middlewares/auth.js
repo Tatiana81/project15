579f97be7a2afe0aa11444a9b2b1a6a5acaf249a
Tatiana <kriista@mail.ru>
2020-05-18 00:59:02 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { cookies } = req;
   const authorization = cookies.jwt;
@@ -10,7 +12,7 @@ module.exports = (req, res, next) => {
   }
   let payload;
   try {
-    payload = jwt.verify(authorization, 'dev-secret');
+    payload = jwt.verify(authorization, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
     console.log('payload: ', payload);
   } catch (err) {
     return res
