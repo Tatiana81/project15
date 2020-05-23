e91667aaf8e638e86d16a364abb9df508f167f68
Tatiana <kriista@mail.ru>
2020-05-23 22:53:51 +0300

    mistakes corrected #2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 require('dotenv').config();
@@ -20,13 +19,8 @@ mongoose.connect(DATABASE_URL, {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(requestLogger);
 
 app.use(router);
-
-app.use(errorLogger);
-
-app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const { celebrate, Joi } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
 const users = require('./users');
 const cards = require('./cards');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/not-found-err');
 const { createUser, login } = require('../controllers/users');
 
+const { requestLogger, errorLogger } = require('../middlewares/logger');
+
 const router = express.Router();
 
 router
+  .use(requestLogger)
+  .post('/signin', celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }), login)
+  .post('/signup', celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required().uri(),
+      email: Joi.string().required().email(),
+      password: Joi.string().required().min(8),
+    }),
+  }), createUser)
   .use('/cards', celebrate({
     cookies: Joi.object().keys({
       jwt: Joi.string().length(172),
@@ -25,21 +43,8 @@ router
   .use('*', (req, res, next) => {
     next(new NotFoundError('Запрашиваемый ресурс не найден'));
   })
-  .post('/signin', celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }), login)
-  .post('/signup', celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().required().uri(),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
-    }),
-  }), createUser)
+  .use(errorLogger)
+  .use(errors())
   .use((err, req, res) => {
     if (!err.statusCode) res.status(500).send({ message: 'Ошибка сервера' });
     res.status(err.statusCode).send({ message: err.message });
