b5e64fd54d8f7f13bdad2b6054bf759389dcc8a3
Tatiana <kriista@mail.ru>
2020-05-23 23:59:47 +0300

    status code error mistake corrected

--- routes/index.js
+++ routes/index.js
@@ -46,8 +46,14 @@ router
   .use(errorLogger)
   .use(errors())
   .use((err, req, res) => {
-    if (!err.statusCode) res.status(500).send({ message: 'Ошибка сервера' });
-    res.status(err.statusCode).send({ message: err.message });
+    const { statusCode = 500, message } = err;
+    res
+      .status(statusCode)
+      .send({
+        message: statusCode === 500
+          ? 'На сервере произошла ошибка'
+          : message,
+      });
   });
 
 module.exports = router;
