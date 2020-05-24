0b9811f9fd6babcabeccc92eb4f0fbde900455e8
Tatiana <kriista@mail.ru>
2020-05-24 12:10:57 +0300

    routers refactoring

--- routes/cards.js
+++ routes/cards.js
@@ -1,53 +1,54 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 const auth = require('../middlewares/auth');
 
 const {
   getCards, deleteCard, createCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-}), getCards);
-
-router.post('/', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  body: Joi.object().keys({
-    name: Joi.string().required().alphanum().min(2)
-      .max(30),
-    link: Joi.string().required().uri(),
-  }),
-}), auth, createCard);
-
-router.delete('/:cardId', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }),
-}), auth, deleteCard);
-
-router.put('/:cardId/likes', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }),
-}), likeCard);
-
-router.delete('/:cardId/likes', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }),
-}), dislikeCard);
+router
+  .use(requestLogger)
+  .get('/', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+  }), getCards)
+  .post('/', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    body: Joi.object().keys({
+      name: Joi.string().required().alphanum().min(2)
+        .max(30),
+      link: Joi.string().required().uri(),
+    }),
+  }), auth, createCard)
+  .delete('/:cardId', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+  }), auth, deleteCard)
+  .put('/:cardId/likes', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+  }), likeCard)
+  .delete('/:cardId/likes', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    params: Joi.object().keys({
+      cardId: Joi.string().alphanum().length(24),
+    }),
+  }), dislikeCard)
+  .use(errorLogger)
+  .use(errors());
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,47 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 
 const {
   findUser, findAllUsers, updateUser, updateAvatar,
 } = require('../controllers/users');
 
-router.get('/:userId', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
-  }),
-}), findUser);
-
-router.get('/', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-}), findAllUsers);
-
-router.patch('/me', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  body: Joi.object().keys({
-    name: Joi.string().required().alphanum().min(2)
-      .max(30),
-    about: Joi.string().required().alphanum().min(2)
-      .max(30),
-    avatar: Joi.string().required().uri(),
-  }),
-}), updateUser);
-
-router.patch('/me/avatar', celebrate({
-  cookies: Joi.object().keys({
-    jwt: Joi.string().length(172),
-  }).unknown(true),
-  body: Joi.object().keys({
-    avatar: Joi.string().required().uri(),
-  }),
-}), updateAvatar);
+router
+  .use(requestLogger)
+  .get('/:userId', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    params: Joi.object().keys({
+      userId: Joi.string().alphanum().length(24),
+    }),
+  }), findUser)
+  .get('/', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+  }), findAllUsers)
+  .patch('/me', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    body: Joi.object().keys({
+      name: Joi.string().required().alphanum().min(2)
+        .max(30),
+      about: Joi.string().required().alphanum().min(2)
+        .max(30),
+      avatar: Joi.string().required().uri(),
+    }),
+  }), updateUser)
+  .patch('/me/avatar', celebrate({
+    cookies: Joi.object().keys({
+      jwt: Joi.string().length(172),
+    }).unknown(true),
+    body: Joi.object().keys({
+      avatar: Joi.string().required().uri(),
+    }),
+  }), updateAvatar)
+  .use(errorLogger)
+  .use(errors());
 
 module.exports = router;
